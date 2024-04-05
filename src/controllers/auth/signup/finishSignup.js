546834612aaccc0e3d539842e34754d9af8599b0
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-05 11:33:19 +0530

    finish signup step

---
+++ src/controllers/auth/signup/finishSignup.js
@@ -0,0 +1,29 @@
+const userModal = require("#db/models/user");
+const jwt = require('jsonwebtoken')
+async function finishSignup(req, res) {
+    const { user } = req.body
+    try {
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        const dataSet = {
+            token: token
+        }
+        if (req.body.profile_pic) {
+            dataSet['profilePicture'] = req.body.profile_pic;
+        } else {
+            dataSet['profilePicture'] = 'https://res.cloudinary.com/dkajmiasf/image/upload/v1712255876/zhc3ijrxyrmlbczalf5h.png'
+        }
+
+        const updated = await userModal.updateOne({ _id: user?._id, isVerified: true }, { dataSet })
+        res.status(200).json({
+            message: "Sign up successful",
+            status: true,
+            authToken: token
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: error?.message,
+            status: false
+        });
+    }
+}
+module.exports = finishSignup
