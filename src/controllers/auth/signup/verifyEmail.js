f727f72ec4c7526a7d556f5b0f658cd48d55113e
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-05 14:59:33 +0530

    "Update verifyEmail success message and OTP validation error

--- src/controllers/auth/signup/verifyEmail.js
+++ src/controllers/auth/signup/verifyEmail.js
@@ -3,19 +3,21 @@ const jwt = require("jsonwebtoken");
 async function verifyEmail(req, res) {
     try {
         const { otp, user } = req.body
+        // Check if the provided OTP matches the OTP stored in the database
         if (otp == user.otp) {
             const verifed = await userModal.updateOne({ _id: user._id }, { $set: { otp: null } })
             res.status(200).json({
                 status: true,
-                message: "Verification Successful",
+                message: "Congratulations! You've unlocked the mystery door of verification!",
             })
         } else {
-            throw new Error('Invalid OTP')
+            // Throw an error if the OTP is invalid
+            throw new Error("Oops! Wrong secret code entered!")
         }
     } catch (error) {
         res.status(400).json({
             dev_msg: error?.message,
-            message: 'Invalid OTP',
+            error: "Oops! Wrong secret code entered!",
             status: false
         })
     }
