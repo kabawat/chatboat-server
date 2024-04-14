a2aa669f9f17682401823078ec6e89b27893e0f0
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-14 22:03:04 +0530

    try to resolve socket cors error

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ const connectDB = require('#config/databaseConfig')
 const router = require('#routers/index');
 const socket_login = require('#src/socket/login');
 const app = expres()
+const { PORT } = process.env || 2917
+app.get("/", (req, res) => {
+    res.send(`<a href='https://kabawat.com'>welcome to kabawat studio</a>`)
+})
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors(
+    { origin: ["http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:5173"] }
+))
+
+app.use(fileUpload())
+app.use('/api', router) // Set up our API endpoint, which points to the
+
+let connectedClients = {};
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
@@ -16,35 +30,26 @@ const io = new Server(httpServer, {
     }
 });
 
-const { PORT } = process.env || 2917
-connectDB()
-app.get("/", (req, res) => {
-    res.send(`<a href='https://kabawat.com'>welcome to kabawat studio</a>`)
-})
-app.use(expres.json()) // Parse incoming requests data as JSON
-app.use(expres.urlencoded({ extended: true }))
-app.use(cors())
-app.use(fileUpload())
-app.use('/api', router) // Set up our API endpoint, which points to the
+const startSocketServer = () => {
+    io.on("connection", (socket) => {
+        socket.on('login', (data) => {
+            console.log("data : ", data)
+            connectedClients[socket.id] = data?.username;
+            socket_login(socket, data)
+        });
 
-let connectedClients = {};
+        // Listen for disconnect event
+        socket.on('disconnect', () => {
+            if (connectedClients[socket.id]) {
+                console.log(`${connectedClients[socket.id]} disconnected.`);
+                delete connectedClients[socket.id];
+            }
+        });
+    });
+}
 
 httpServer.listen(PORT, () => {
+    connectDB()
+    startSocketServer()
     console.log(`http://localhost:${PORT}`)
 })
-
-io.on("connection", (socket) => {
-    socket.on('login', (data) => {
-        console.log("data : ", data)
-        connectedClients[socket.id] = data?.username;
-        socket_login(socket, data)
-    });
-
-    // Listen for disconnect event
-    socket.on('disconnect', () => {
-        if (connectedClients[socket.id]) {
-            console.log(`${connectedClients[socket.id]} disconnected.`);
-            delete connectedClients[socket.id];
-        }
-    });
-});
