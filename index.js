249b8b47f74e968e0eda341c6884badf8d805bdb
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-14 21:49:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = expres()
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: ["http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:5173"]
     }
 });
 
