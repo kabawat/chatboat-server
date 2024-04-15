e40d283a3fe7db1123746dc22afe3e5ee8113784
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-15 10:58:44 +0530

    try to resolve cors error

--- index.js
+++ index.js
@@ -9,47 +9,40 @@ const router = require('#routers/index');
 const socket_login = require('#src/socket/login');
 const app = express()
 const { PORT } = process.env || 2917
-connectDB()
 app.get("/", (req, res) => {
     res.send(`<a href='https://kabawat.com'>welcome to kabawat studio</a>`)
 })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors(
-    { origin: ["https://chatboat-gamma.vercel.app", "http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:5173"] }
-))
+app.use(cors({ origin: '*' }));
 
 app.use(fileUpload())
 app.use('/api', router) // Set up our API endpoint, which points to the
-
+connectDB()
 let connectedClients = {};
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: ["https://chatboat-gamma.vercel.app", "http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:5173"]
-    }
+    cors: { origin: '*' }
 });
 
-const startSocketServer = () => {
-    io.on("connection", (socket) => {
-        socket.on('login', (data) => {
-            console.log("data : ", data)
-            connectedClients[socket.id] = data?.username;
-            socket_login(socket, data)
-        });
-
-        // Listen for disconnect event
-        socket.on('disconnect', () => {
-            if (connectedClients[socket.id]) {
-                console.log(`${connectedClients[socket.id]} disconnected.`);
-                delete connectedClients[socket.id];
-            }
-        });
-    });
-}
 
 httpServer.listen(PORT, () => {
-    startSocketServer()
     console.log(`http://localhost:${PORT}`)
 })
+
+io.on("connection", (socket) => {
+    socket.on('login', (data) => {
+        console.log("data : ", data)
+        connectedClients[socket.id] = data?.username;
+        socket_login(socket, data)
+    });
+
+    // Listen for disconnect event
+    socket.on('disconnect', () => {
+        if (connectedClients[socket.id]) {
+            console.log(`${connectedClients[socket.id]} disconnected.`);
+            delete connectedClients[socket.id];
+        }
+    });
+});
