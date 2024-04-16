e20f57d5f03b77a9276c78ef8dcd6347db2a44e4
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-16 12:17:52 +0530

    Update dependencies, add cors origin configuration, and move socket server code

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
-require('dotenv').config() // Import the dotenv module to load environment variables from a.env file
 const express = require('express')
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+const http = require('http')
+const { Server } = require('socket.io')
+const app = express()
+// const { userModal } = require('./controller')
 const fileUpload = require('express-fileupload')
-const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+const dot = require('dotenv').config()
 const connectDB = require('#config/databaseConfig')
 const router = require('#routers/index');
 const socket_login = require('#src/socket/login');
-const app = express()
-const { PORT } = process.env || 2917
+const cors = require('cors')
+const port = process.env.PORT || 2917
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors(
+    { origin: ['http://localhost:3000', 'https://query-boat.onrender.com', 'https://blissful-shadow-99347.pktriot.net'] }
+))
+app.use(bodyParser.json())
+app.use(fileUpload())
 app.get("/", (req, res) => {
     res.send(`<a href='https://kabawat.com'>welcome to kabawat studio</a>`)
 })
+app.use('/api', router)
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: '*' }));
 
-app.use(fileUpload())
-app.use('/api', router) // Set up our API endpoint, which points to the
-connectDB()
+const server = http.createServer(app)
 let connectedClients = {};
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-    cors: { origin: '*' }
-});
-
-
-httpServer.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:3000', 'https://query-boat.onrender.com', 'https://blissful-shadow-99347.pktriot.net']
+    }
 })
 
-io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+// socket data 
+const startSocketServer = () => {
+    io.on("connection", (socket) => {
+        console.log("a user connected", socket.id);
 
-    // Listen for login event
-    socket.on('login', (data) => {
-        console.log("data : ", data)
-        connectedClients[socket.id] = data?.username;
-        socket_login(socket, data)
-    });
+        // Listen for login event
+        socket.on('login', (data) => {
+            console.log("data : ", data)
+            connectedClients[socket.id] = data?.username;
+            socket_login(socket, data)
+        });
 
-    // Listen for disconnect event
-    socket.on('disconnect', () => {
-        if (connectedClients[socket.id]) {
-            console.log(`${connectedClients[socket.id]} disconnected.`);
-            delete connectedClients[socket.id];
-        }
+        // Listen for disconnect event
+        socket.on('disconnect', () => {
+            if (connectedClients[socket.id]) {
+                console.log(`${connectedClients[socket.id]} disconnected.`);
+                delete connectedClients[socket.id];
+            }
+        });
     });
-});
+};
+
+// server listen 
+server.listen(port, async () => {
+    startSocketServer()
+    await connectDB()
+    console.log(`http://localhost:${port}`)
+})
