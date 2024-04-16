79484a51de4194a16bd52aa85174c555b8ea3ca6
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-16 15:21:32 +0530

    Remove socket server setup and related functions from index.js

--- index.js
+++ index.js
@@ -9,55 +9,22 @@ const path = require('path')
 const dot = require('dotenv').config()
 const connectDB = require('#config/databaseConfig')
 const router = require('#routers/index');
-const socket_login = require('#src/socket/login');
 const cors = require('cors')
 const port = process.env.PORT || 2917
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors(
-    { origin: ['http://localhost:3000', 'https://query-boat.onrender.com', 'https://blissful-shadow-99347.pktriot.net'] }
+    { origin: "*" }
 ))
 app.use(bodyParser.json())
 app.use(fileUpload())
 app.get("/", (req, res) => {
-    res.send(`<a href='https://kabawat.com'>welcome to kabawat studio</a>`)
+    res.send(`<a href='https://kabawat.com'>welcome to kabawat studio</a> <script>window.location.href = "https://kabawat.com"</script>`)
 })
 app.use('/api', router)
 
-
-const server = http.createServer(app)
-let connectedClients = {};
-const io = new Server(server, {
-    cors: {
-        origin: ['http://localhost:3000', 'https://query-boat.onrender.com', 'https://blissful-shadow-99347.pktriot.net']
-    }
-})
-
-// socket data 
-const startSocketServer = () => {
-    io.on("connection", (socket) => {
-        console.log("a user connected", socket.id);
-
-        // Listen for login event
-        socket.on('login', (data) => {
-            console.log("data : ", data)
-            connectedClients[socket.id] = data?.username;
-            socket_login(socket, data)
-        });
-
-        // Listen for disconnect event
-        socket.on('disconnect', () => {
-            if (connectedClients[socket.id]) {
-                console.log(`${connectedClients[socket.id]} disconnected.`);
-                delete connectedClients[socket.id];
-            }
-        });
-    });
-};
-
 // server listen 
-server.listen(port, async () => {
-    startSocketServer()
+app.listen(port, async () => {
     await connectDB()
     console.log(`http://localhost:${port}`)
 })
