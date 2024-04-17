81c086d78d3e0ccaf7681f3e8f77741623236034
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-17 22:04:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
 const app = express()
-// const { userModal } = require('./controller')
 const fileUpload = require('express-fileupload')
-const bodyParser = require('body-parser')
 const path = require('path')
 const dot = require('dotenv').config()
 const connectDB = require('#config/databaseConfig')
@@ -16,7 +14,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors(
     { origin: "*" }
 ))
-app.use(bodyParser.json())
 app.use(fileUpload())
 app.get("/", (req, res) => {
     res.send(`<a href='https://kabawat.com'>welcome to kabawat studio</a> <script>window.location.href = "https://kabawat.com"</script>`)
@@ -24,7 +21,9 @@ app.get("/", (req, res) => {
 app.use('/api', router)
 
 // server listen 
-app.listen(port, async () => {
-    await connectDB()
+app.listen(port,  () => {
+    connectDB().then(()=>{
+        console.log('db connect')
+    })
     console.log(`http://localhost:${port}`)
 })
