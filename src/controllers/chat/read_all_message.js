dc6f8fa56fe57fa0ce512c2ebe332d5e33669062
kabawat <118162167+kabawat@users.noreply.github.com>
2024-05-16 10:52:14 +0530

    Refactor mark_read_all_message function: updated variable extraction and removed response data

--- src/controllers/chat/read_all_message.js
+++ src/controllers/chat/read_all_message.js
@@ -3,10 +3,9 @@ const sendNotification = require("#root/src/web-hooks/slack");
 
 async function mark_read_all_message(req, res) {
     try {
-        // Extract chatID and userID from the request body
-        const { chatID, userID } = req.body
+        const chatID = req.body?.chat_id;
+        const userID = req.body?.userID;
 
-        // Update documents where userID is not in the mark_as_read array
         const result = await chatModal.updateMany(
             { chat_id: chatID, mark_as_read: { $nin: [userID] } },
             { $push: { mark_as_read: userID } }
@@ -14,10 +13,8 @@ async function mark_read_all_message(req, res) {
 
         res.status(200).json({
             status: true,
-            data: result // Response containing the updated documents
         });
     } catch (error) {
-        // Handle error and send notification
         sendNotification(error, 'mark_read_all_message', req?.body);
         res.status(500).json({
             status: false,
